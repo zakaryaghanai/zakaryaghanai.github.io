44361e47d88e013ffdc960b832bac7f5d011734c
zakariya ghanai <zakariyaghanai@gmail.com>
2022-03-28 10:26:25 +0100

    add auto selected nav link script

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -2,4 +2,27 @@ $(document).ready(function () {
     if (CSS && 'paintWorklet' in CSS) {
         CSS.paintWorklet.addModule('https://unpkg.com/smooth-corners')
     }
+
+    $(window).scroll( function () {
+        var current = "";
+
+        $('section').each(function (){
+            const sectionTop = $(this).offset().top;
+            const sectionHeight = $(this).height();
+
+
+            if ( $(window).scrollTop() >= sectionTop - (sectionHeight /2) ) {
+                current = $(this).attr("id");
+            }
+        });
+
+        $('nav ul a').each(function () {
+            $(this).removeClass("active");
+            if ($(this).hasClass(current)) {
+                $(this).addClass("active");
+            }
+        });
+    });
+
+    $(window).scroll()
 })
