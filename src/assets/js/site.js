dc1d6d1c26fea5b0c9df4da307058f7c9340a93c
zakariya ghanai <zakariyaghanai@gmail.com>
2022-06-25 17:15:46 +0100

    refactor UI

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -12,17 +12,29 @@ $(document).ready(function () {
 
 
             if ( $(window).scrollTop() >= sectionTop - (sectionHeight /2) ) {
-                current = $(this).attr("id");
+                current = $(this).attr("id")
             }
         });
 
-        $('nav ul a').each(function () {
-            $(this).removeClass("active");
-            if ($(this).hasClass(current)) {
-                $(this).addClass("active");
-            }
-        });
+        // $('nav ul a').each(function () {
+        //     $(this).removeClass("active")
+        //     if ($(this).attr('href') == '#' + current) {
+        //         $(this).addClass('active')
+        //     }
+        // });
     });
 
+    $('.logo a').click(function() {       
+        $('nav ul a').each(function () {
+
+            $(this).removeClass("active");
+        });
+    })
+    
     $(window).scroll()
+
+    $('#viewResume').click(function() {
+        let resumeUrl = $('[data-resume-url]').attr('data-resume-url');
+       window.open(resumeUrl, '_blank');
+    })
 })
